6416d102d481e1dd1fbb7aad09e6b4a3dd454030
Márton Dávid Orosz <mdox@mdox.xyz>
2023-04-16 12:35:59 +0200

    ADD: UI Setup: TailwindCSS, Flowbite, Inter font

---
+++ ki_vv-frontend/tailwind.config.js
@@ -0,0 +1,18 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+		"node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+	],
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ["var(--inter)", ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [require("flowbite/plugin")],
+};
